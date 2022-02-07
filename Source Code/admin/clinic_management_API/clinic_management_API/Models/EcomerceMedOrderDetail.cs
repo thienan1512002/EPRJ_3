5abe7d828a392837d8a8bc502669db8d9774f578
thienan1512002 <nhta151202@gmail.com>
2022-02-07 21:17:41 +0700

    RestAPI StaffAccount

---
+++ Source Code/admin/clinic_management_API/clinic_management_API/Models/EcomerceMedOrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace clinic_management_API.Models
+{
+    public partial class EcomerceMedOrderDetail
+    {
+        public int OrderId { get; set; }
+        public int? OrderDetailId { get; set; }
+        public string MedId { get; set; }
+        public int? Quantity { get; set; }
+        public int? Total { get; set; }
+
+        public virtual Medicine Med { get; set; }
+        public virtual EcomerceOrder OrderDetail { get; set; }
+    }
+}
---
+++ Source Code/admin/clinic_management_API/clinic_management_API/Models/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace clinic_management_API.Models
+{
+    public partial class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public string CourseId { get; set; }
+        public string AccountId { get; set; }
+
+        public virtual StaffAccount Account { get; set; }
+        public virtual Course Course { get; set; }
+    }
+}
