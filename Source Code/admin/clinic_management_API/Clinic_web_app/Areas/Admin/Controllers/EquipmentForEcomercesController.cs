cf3babcf308650779ea0960376f5e58ef64342d5
thienan1512002 <72856151+thienan1512002@users.noreply.github.com>
2022-02-09 21:12:32 +0700

    EPRJ_3 | Upload Data | An
    chỉ xong 1/3

---
+++ Source Code/admin/clinic_management_API/Clinic_web_app/Areas/Admin/Controllers/EquipmentForEcomercesController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Clinic_web_app.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace Clinic_web_app.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class EquipmentForEcomercesController : Controller
+    {
+        private readonly ClinicDBContext _context;
+
+        public EquipmentForEcomercesController(ClinicDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/EquipmentForEcomerces
+        public async Task<IActionResult> Index()
+        {
+            var clinicDBContext = _context.EquipmentForEcomerces.Include(e => e.Brand);
+            return View(await clinicDBContext.ToListAsync());
+        }
+
+        // GET: Admin/EquipmentForEcomerces/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipmentForEcomerce = await _context.EquipmentForEcomerces
+                .Include(e => e.Brand)
+                .FirstOrDefaultAsync(m => m.EquipmentId == id);
+            if (equipmentForEcomerce == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipmentForEcomerce);
+        }
+
+        // GET: Admin/EquipmentForEcomerces/Create
+        public IActionResult Create()
+        {
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandName");
+            return View();
+        }
+
+        // POST: Admin/EquipmentForEcomerces/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(EquipmentForEcomerce equipmentForEcomerce , IFormFile file)
+        {
+            if (ModelState.IsValid)
+            {
+                if (file != null)
+                {
+                    string fileName = Path.GetFileName(file.FileName);
+                    string filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot/images/", fileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    equipmentForEcomerce.Image = "images/" + fileName;
+                }
+                _context.Add(equipmentForEcomerce);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandId", equipmentForEcomerce.BrandId);
+            return View(equipmentForEcomerce);
+        }
+
+        // GET: Admin/EquipmentForEcomerces/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipmentForEcomerce = await _context.EquipmentForEcomerces.FindAsync(id);
+            if (equipmentForEcomerce == null)
+            {
+                return NotFound();
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandName", equipmentForEcomerce.BrandId);
+            return View(equipmentForEcomerce);
+        }
+
+        // POST: Admin/EquipmentForEcomerces/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, EquipmentForEcomerce equipmentForEcomerce , IFormFile file)
+        {
+            if (id != equipmentForEcomerce.EquipmentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (file != null)
+                    {
+                        string fileName = Path.GetFileName(file.FileName);
+                        string filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot/images/", fileName);
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+                        equipmentForEcomerce.Image = "images/" + fileName;
+                    }
+                    _context.Update(equipmentForEcomerce);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EquipmentForEcomerceExists(equipmentForEcomerce.EquipmentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandName", equipmentForEcomerce.BrandId);
+            return View(equipmentForEcomerce);
+        }
+
+        // GET: Admin/EquipmentForEcomerces/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipmentForEcomerce = await _context.EquipmentForEcomerces
+                .Include(e => e.Brand)
+                .FirstOrDefaultAsync(m => m.EquipmentId == id);
+            if (equipmentForEcomerce == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipmentForEcomerce);
+        }
+
+        // POST: Admin/EquipmentForEcomerces/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var equipmentForEcomerce = await _context.EquipmentForEcomerces.FindAsync(id);
+            _context.EquipmentForEcomerces.Remove(equipmentForEcomerce);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EquipmentForEcomerceExists(string id)
+        {
+            return _context.EquipmentForEcomerces.Any(e => e.EquipmentId == id);
+        }
+    }
+}
---
+++ Source Code/admin/clinic_management_API/Clinic_web_app/Areas/Admin/Controllers/MedicinesAdminController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Clinic_web_app.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace Clinic_web_app.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MedicinesAdminController : Controller
+    {
+        private readonly ClinicDBContext _context;
+
+        public MedicinesAdminController(ClinicDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/MedicinesAdmin
+        public async Task<IActionResult> Index()
+        {
+            var clinicDBContext = _context.Medicines.Include(m => m.Brand);
+            return View(await clinicDBContext.ToListAsync());
+        }
+
+        // GET: Admin/MedicinesAdmin/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medicine = await _context.Medicines
+                .Include(m => m.Brand)
+                .FirstOrDefaultAsync(m => m.MedId == id);
+            if (medicine == null)
+            {
+                return NotFound();
+            }
+
+            return View(medicine);
+        }
+
+        // GET: Admin/MedicinesAdmin/Create
+        public IActionResult Create()
+        {
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandId");
+            return View();
+        }
+
+        // POST: Admin/MedicinesAdmin/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create( Medicine medicine , IFormFile file)
+        {
+            if (ModelState.IsValid)
+            {
+                if (file != null)
+                {
+                    string fileName = Path.GetFileName(file.FileName);
+                    string filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot/images/", fileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    medicine.Image = "images/" + fileName;
+                }
+                _context.Add(medicine);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandName", medicine.BrandId);
+            return View(medicine);
+        }
+
+        // GET: Admin/MedicinesAdmin/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medicine = await _context.Medicines.FindAsync(id);
+            if (medicine == null)
+            {
+                return NotFound();
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandName", medicine.BrandId);
+            return View(medicine);
+        }
+
+        // POST: Admin/MedicinesAdmin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id,  Medicine medicine , IFormFile file)
+        {
+            if (id != medicine.MedId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (file != null)
+                    {
+                        string fileName = Path.GetFileName(file.FileName);
+                        string filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot/images/", fileName);
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+                        medicine.Image = "images/" + fileName;
+                    }
+                    _context.Update(medicine);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MedicineExists(medicine.MedId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandId", medicine.BrandId);
+            return View(medicine);
+        }
+
+        // GET: Admin/MedicinesAdmin/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medicine = await _context.Medicines
+                .Include(m => m.Brand)
+                .FirstOrDefaultAsync(m => m.MedId == id);
+            if (medicine == null)
+            {
+                return NotFound();
+            }
+
+            return View(medicine);
+        }
+
+        // POST: Admin/MedicinesAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var medicine = await _context.Medicines.FindAsync(id);
+            _context.Medicines.Remove(medicine);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MedicineExists(string id)
+        {
+            return _context.Medicines.Any(e => e.MedId == id);
+        }
+    }
+}
