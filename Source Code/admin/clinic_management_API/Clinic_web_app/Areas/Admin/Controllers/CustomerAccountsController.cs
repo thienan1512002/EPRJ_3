7ed76bbb2c132e423c60e5d3d8496a0f0abed5c0
thienan1512002 <72856151+thienan1512002@users.noreply.github.com>
2022-02-10 18:08:29 +0700

    Fix ISS , LoadData Customer on DashBoard

---
+++ Source Code/admin/clinic_management_API/Clinic_web_app/Areas/Admin/Controllers/CustomerAccountsController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Clinic_web_app.Models;
+
+namespace Clinic_web_app.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CustomerAccountsController : Controller
+    {
+        private readonly ClinicDBContext _context;
+
+        public CustomerAccountsController(ClinicDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/CustomerAccounts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.CustomerAccounts.ToListAsync());
+        }
+
+        // GET: Admin/CustomerAccounts/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customerAccount = await _context.CustomerAccounts
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customerAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(customerAccount);
+        }
+
+        // GET: Admin/CustomerAccounts/Create
+
+        // GET: Admin/CustomerAccounts/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customerAccount = await _context.CustomerAccounts.FindAsync(id);
+            if (customerAccount == null)
+            {
+                return NotFound();
+            }
+            return View(customerAccount);
+        }
+
+        // POST: Admin/CustomerAccounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("CustomerId,CustomerName,Email,Password,Phone,Address,Status")] CustomerAccount customerAccount)
+        {
+            if (id != customerAccount.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customerAccount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customerAccount);
+        }
+    }
+}
