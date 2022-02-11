830dfc6093ab255c7b2fef2b841ef0632c7f41b6
thienan1512002 <72856151+thienan1512002@users.noreply.github.com>
2022-02-11 10:57:06 +0700

    Staffaccount Login (50% finished)

--- Source Code/admin/clinic_management_API/Clinic_web_app/Areas/Admin/Controllers/HomeController.cs
+++ Source Code/admin/clinic_management_API/Clinic_web_app/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Clinic_web_app.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,11 @@ namespace Clinic_web_app.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly ClinicDBContext _context;
+        public HomeController(ClinicDBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -37,6 +44,29 @@ namespace Clinic_web_app.Areas.Admin.Controllers
         public IActionResult CustomerAccount()
         {
             return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost, ActionName("Login")]
+        [ValidateAntiForgeryToken]
+        public IActionResult ProcessLogin(string username, string password)
+        {
+            if (username.Equals("") && password.Equals(""))
+            {
+                return View();
+            }
+            var staffAccount = _context.StaffAccounts.SingleOrDefault(account => account.Username.Equals(username) && account.Password.Equals(password));
+            if (staffAccount == null)
+            {
+                return View();
+                ViewBag.Message = "Incorect username or Password";
+            }
+            HttpContext.Session.SetString("accountId", staffAccount.AccountId);
+            HttpContext.Session.SetString("accountName", staffAccount.Username);
+            HttpContext.Session.SetString("avatar", staffAccount.Image);
+            return RedirectToAction("Index");
         }
     }
 }
